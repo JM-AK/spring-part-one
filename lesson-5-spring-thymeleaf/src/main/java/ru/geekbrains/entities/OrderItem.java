e79032e807bb6e549e4508f7a7cdbde5e13398b1
JM-AK <komlevalexandr87@gmail.com>
2020-12-17 18:18:53 +0300

    [lesson 5] repeat lesson - start

---
+++ lesson-5-spring-thymeleaf/src/main/java/ru/geekbrains/entities/OrderItem.java
@@ -0,0 +1,35 @@
+package ru.geekbrains.entities;
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import java.math.BigDecimal;
+
+@Entity
+@Table(name = "order_items")
+@Data
+@NoArgsConstructor
+public class OrderItem {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "product_id")
+    private Product product;
+
+    @ManyToOne
+    @JoinColumn(name = "order_id")
+    private Order order;
+
+    @Column(name = "quantity")
+    private int quantity;
+
+    @Column(name = "price_per_product")
+    private BigDecimal pricePerProduct;
+
+    @Column(name = "price")
+    private BigDecimal price;
+}
