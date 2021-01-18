38605a25c3123666a03f6724a7a605499102b551
JM-AK <komlevalexandr87@gmail.com>
2021-01-18 18:02:38 +0300

    [lesson-7-8] add flyway config

---
+++ lesson-8-spring-rest-security/src/main/resources/db/migration/V1.0__initialize.sql
@@ -0,0 +1,49 @@
+create table customers (
+    id                      bigserial,
+    name                    varchar(255) not null,
+    primary key (id)
+);
+
+create table products (
+    id                      bigserial primary key,
+    title                   varchar(255),
+    price                   int
+);
+
+create table orders (
+    id                      bigserial primary key,
+    customer_id             bigint references customers(id),
+    price                   int
+);
+
+create table order_items (
+    id                      bigserial primary key,
+    product_id              bigint references products(id),
+    order_id                bigint references orders(id),
+    price                   int,
+    price_per_product       int,
+    quantity                int
+);
+
+insert into customers (name)
+values
+('Bob'),
+('John'),
+('Jack');
+
+insert into products (title, price)
+values
+('Bread1', 21),
+('Bread2', 22),
+('Bread3', 23),
+('Bread4', 24),
+('Bread5', 25),
+('Bread6', 26),
+('Bread7', 27),
+('Bread8', 28),
+('Bread9', 29),
+('Bread10', 30),
+('Bread11', 31),
+('Bread12', 32),
+('Bread13', 33),
+('Bread14', 34);
